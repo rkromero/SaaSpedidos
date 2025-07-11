6f6211548fdc07ad24b4df081dceb33f66f00320
unknown <rodolfor86@gmail.com>
2025-07-11 14:03:57 -0300

    FIX CSS CRÍTICO: Corregir calc(auto + env()) inválido que causaba error de build en Railway

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -2,5 +2,12 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-calc': {
+      warnWhenCannotResolve: true,
+      precision: 5,
+      preserve: true,
+      selectors: true,
+      mediaQueries: true
+    }
   },
 } 
